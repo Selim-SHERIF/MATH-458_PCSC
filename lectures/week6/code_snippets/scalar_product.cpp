21ca86dcc6de2a04911654f0a621284e33cda80d
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2024-10-16 16:06:45 +0200

    add another snippet showing primer on type traites

--- lectures/week6/code_snippets/scalar_product.cpp
+++ lectures/week6/code_snippets/scalar_product.cpp
@@ -21,3 +21,34 @@ int main() {
   double res = scalarProduct(vector1_3d, vector2_3d);
   std::cout << res << std::endl;
 }
+
+/////////////////
+// another possibility as shown in class :
+// introduction to type traits
+// => embeds accissible information(type, dim) in struct
+///////////////7/
+
+// specialization for vectors
+template <typename T1, typename T2>
+typename T1::value_type scalarProduct2(const T1 &v1, const T2 &v2) {
+
+  typename T1::value_type res;
+  for (int d = 0; d < T1::_dim; ++d)
+    res += v1[d] * v2[d];
+  return res;
+}
+
+template <typename T = double, int dim = 3> struct MyVector2 {
+
+  T operator[](int i) const { return values[i]; }
+  using value_type = T;
+  static const int _dim = dim;
+
+  T values[dim];
+};
+
+void foo() {
+  MyVector2 v1;
+  MyVector2 v2;
+  auto ret = scalarProduct2(v1, v2);
+}
