9c6bdee9e017505e92aa8a49d2ea7a7d571d4b2d
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2024-10-16 15:52:13 +0200

    adding the exercises for next week + snippet with const expr

--- lectures/week6/code_snippets/arithmetic_series_meta_programming.cpp
+++ lectures/week6/code_snippets/arithmetic_series_meta_programming.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
-template <int i> int arithmetic_template() {
-  return i + arithmetic_template<i - 1>();
-}
+// template <int i> int arithmetic_template() {
+//   return i + arithmetic_template<i - 1>();
+// }
 
-template <> int arithmetic_template<1>() {
-  // ends recursion
-  return 1;
+// template <> int arithmetic_template<1>() {
+//   // ends recursion
+//   return 1;
+// }
+
+// with constexpr no need for specialization
+template <int i> int arithmetic_template() {
+  if constexpr (i == 1) {
+    return 1;
+  } else
+    return i + arithmetic_template<i - 1>();
 }
 
 int main() {
-  std::cout << arithmetic_template<5>();
+  std::cout << arithmetic_template<100>();
   std::cout << std::endl;
 }
